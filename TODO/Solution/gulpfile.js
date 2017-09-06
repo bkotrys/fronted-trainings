6b172d9d7f09c2254db2f02c31ea9962e3a2441d
Kamil Faron <camil.faron@gmail.com>
2017-09-07 00:53:13 +0200

    TODO - Gulp Tutorial, Sass

---
+++ TODO/Solution/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('hello', function() {
+  console.log('Hello Kamil');
+});
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/styles.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
+});
