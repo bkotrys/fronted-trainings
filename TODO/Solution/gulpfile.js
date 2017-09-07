f330b333f4891a5c12839b0001a79b9172c2b6b1
Bartosz Kotrys <bartosz.kotrys@finder.com>
2017-09-07 20:37:00 +0200

    add some scss improvements

--- TODO/Solution/gulpfile.js
+++ TODO/Solution/gulpfile.js
@@ -1,16 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
-gulp.task('default', function() {
-  // place code for your default task here
-});
-
-gulp.task('hello', function() {
-  console.log('Hello Kamil');
-});
+var watchSass = require('gulp-watch-sass');
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/styles.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('app/build/css'))
+});
+
+gulp.task("sass:watch", () => {
+  gulp.watch([
+    "./app/**/*.scss",
+  ], ["sass"]);
 });
