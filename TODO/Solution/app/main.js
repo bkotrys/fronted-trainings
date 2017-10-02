db530befc53e0604ce70c4a47269934056b11104
Bartosz Kotrys <bartosz.kotrys@finder.com>
2017-10-02 18:50:22 +0200

    update lesson

--- TODO/Solution/app/main.js
+++ TODO/Solution/app/main.js
@@ -1,7 +1,58 @@
-var unique = require('uniq');
+//
+export { Person } from './main';
 
-var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+// variables
+import './main2'
 
-console.log(unique(data));
+export default function Person(name, surname, gender = 'male') {
+  name = name;
+  this.name = name;
+  this.surname = surname;
+  this.creationTime = new Date();
+  this.gender = gender;
+  
+  // this.male = gender !== undefined ? "male" : gender;
 
-var test222;
+  this.fullname = function () {
+    return this.name + " " + this.surname;
+  }
+
+
+  console.log("context person", this);
+
+  document.addEventListener('click', onClick.bind);
+
+  document.addEventListener('click', (event) => onClick(event))
+}
+
+function onClick() {
+  
+  console.log(this);
+}
+var person = new Person('Kamila', 'Faron')
+console.log("Person: ", person)
+
+var family = {
+  "Kamil" : {}
+}
+
+function addPersonToFamily(name, ...params) {
+  family[name] = addPerson(name, ...params)
+}
+
+function addPerson(name, surname, gender) {
+  console.log('Jestem ' + name + " " + surname + ", pleć: " + gender);
+  console.log(`Jestem ${name} ${surname}, płeć: ${gender}`);
+
+  return new Person(name, surname, gender);
+}
+
+addPersonToFamily('Janina', "xyz", 'female');
+console.log(family);
+
+var family = { Kamil: {name: "Kamil"}, InnaOsoba: {}, InnaOsoba2: {}}
+
+var Kamil = family.Kamil;
+var InnaOsoba = family.InnaOsoba;
+
+const { Kamil, InnaOsoba } = family;
--- TODO/Solution/gulpfile.js
+++ TODO/Solution/gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watchSass = require('gulp-watch-sass');
+var browserify = require('gulp-browserify');
+
+// Basic usage 
+gulp.task('browserify', function () {
+  // Single entry point to browserify 
+  gulp.src('app/main.js')
+    .pipe(browserify({
+      insertGlobals: true,
+      debug: true
+    }))
+    .pipe(gulp.dest('./build/js'))
+});
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/styles.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('app/build/css'))
+    .pipe(gulp.dest('./build/css'))
 });
 
 gulp.task("sass:watch", () => {
