1d34a8e72a0d165cdc3c0ea7105fceb19f563032
Bartosz Kotrys <bartosz.kotrys@gmail.com>
2017-10-09 23:06:52 +0200

    refactor todo list

--- TODO/Solution/gulpfile.js
+++ TODO/Solution/gulpfile.js
@@ -4,6 +4,10 @@ var watchSass = require('gulp-watch-sass');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
 
 //Build SASS
 gulp.task('sass', function(){
@@ -12,20 +16,37 @@ gulp.task('sass', function(){
     .pipe(gulp.dest('build'))
 });
 
-//Build Babel and Browserify
-gulp.task('babel-and-browserify', function () {
-    return browserify({entries: 'src/js/app.js', debug: true})
+// //Build Babel and Browserify
+// gulp.task('babel-and-browserify', function () {
+//     return browserify({entries: 'src/js/app.js', debug: true})
+//         .transform("babelify", { presets: ["es2015"] })
+//         .bundle()
+//         .pipe(source('app.js'))
+//         .pipe(gulp.dest('build'));
+// });
+
+gulp.task('browserify', function () {
+    // app.js is your main JS file with all your module inclusions
+    return browserify({ entries: './src/js/app.js', debug: true })
         .transform("babelify", { presets: ["es2015"] })
         .bundle()
         .pipe(source('app.js'))
-        .pipe(gulp.dest('build'));
+        .pipe(buffer())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/js'))
+        .pipe(livereload());
 });
  
 //Watch
 gulp.task('watch', function () {
-    gulp.watch('src/js/*.js', ['babel-and-browserify']);
+    livereload.listen();
+    gulp.watch('src/js/*.js', ['browserify']);
     gulp.watch('src/scss/*.scss', ['sass']);
 });
 
 //Default
 gulp.task('default', ['watch']);
+
+gulp.task('build', ['sass', 'browserify']);
--- TODO/Solution/src/js/app.js
+++ TODO/Solution/src/js/app.js
@@ -1,169 +1,142 @@
-'use strict';
-
-(function () {
+var elementIdCount = 0;
+var buttons = new Array();
 	
-	window.onload = function registerEvents() {
-		
-		//ADDING NEW LIST ELEMENT
-		var form = document.getElementById('form');
-		var elementIdCount = 0;
-		var Buttons = new Array();
+window.onload = function initialize() {
+	registerEvents();		
+}
 
-		form.onsubmit = function(event) {
-			event.preventDefault();
-			var newTaskValue = form.task.value;
-			elementIdCount = elementIdCount+1;
-			
-			//UL
-			var todoList = document.getElementById("todo-list");
-				
-				//LI
-				var newTodoElement = document.createElement("li");
-				newTodoElement.setAttribute("id", `done-todo-element-${elementIdCount}`);
-					
-					//BUTTONS DIV
-					var newTodoElementButtonsDiv = document.createElement("div");
-					newTodoElementButtonsDiv.setAttribute("id", `buttons-div-${elementIdCount}`);
-						
-						//DONE BUTTON
-						var newTodoElementButtonDone = document.createElement("button");
-						newTodoElementButtonDone.setAttribute("type", "button");
-						newTodoElementButtonDone.setAttribute("class", "button done-button");
-						newTodoElementButtonDone.setAttribute("id", `done-button-todo-element-${elementIdCount}`);
-							
-							//DONE ICON AND TEXT
-							var doneIcon = document.createElement("i");
-							doneIcon.setAttribute("class", "fa fa-check-square-o");
-							doneIcon.setAttribute("aria-hidden", "true");
-							newTodoElementButtonDone.appendChild(doneIcon);
-							newTodoElementButtonDone.appendChild(document.createTextNode(" "));
-							newTodoElementButtonDone.appendChild(document.createTextNode("Done"));
-						
-						//EDIT BUTTON
-						var newTodoElementButtonEdit = document.createElement("button");
-						newTodoElementButtonEdit.setAttribute("type", "button");
-						newTodoElementButtonEdit.setAttribute("class", "button edit-button");
-						newTodoElementButtonEdit.setAttribute("id", `edit-button-todo-element-${elementIdCount}`);
-							
-							//EDIT ICON AND TEXT
-							var editIcon = document.createElement("i");
-							editIcon.setAttribute("class","fa fa-pencil-square-o");
-							editIcon.setAttribute("aria-hidden", "true");
-							newTodoElementButtonEdit.appendChild(editIcon);
-							newTodoElementButtonEdit.appendChild(document.createTextNode(" "));
-							newTodoElementButtonEdit.appendChild(document.createTextNode("Edit"));
+function registerEvents() {
+	var form = document.getElementById('form');
+	form.addEventListener("submit", addNewTask)
+}
 
-						//DELETE BUTTON
-						var newTodoElementButtonDelete = document.createElement("button");
-						newTodoElementButtonDelete.setAttribute("type", "button");
-						newTodoElementButtonDelete.setAttribute("class", "button delete-button");
-						newTodoElementButtonDelete.setAttribute("id", `delete-button-todo-element-${elementIdCount}`);
-							
-							//DELETE ICON AND TEXT
-							var deleteIcon = document.createElement("i");
-							deleteIcon.setAttribute("class", "fa fa-minus-square-o");
-							deleteIcon.setAttribute("aria-hidden", "true");
-							newTodoElementButtonDelete.appendChild(deleteIcon);
-							newTodoElementButtonDelete.appendChild(document.createTextNode(' '));
-							newTodoElementButtonDelete.appendChild(document.createTextNode('Delete'));
+function addNewTask (event) {
+	event.preventDefault();
 
-					//ADDING BUTTONS TO BUTTONS DIV
-					newTodoElementButtonsDiv.appendChild(newTodoElementButtonDone);
-					newTodoElementButtonsDiv.appendChild(newTodoElementButtonEdit);
-					newTodoElementButtonsDiv.appendChild(newTodoElementButtonDelete);
+	var newTaskValue = form.task.value;
+	var todoList = document.getElementById("todo-list");
+	var newTodoElement = document.createElement("li");
 
-				//ADDING TASK VALUE AND BUTTONS DIV
-				newTodoElement.appendChild(document.createTextNode(newTaskValue));
-				newTodoElement.appendChild(newTodoElementButtonsDiv);
-			//ADDING ELEMENT TO LIST
-			todoList.appendChild(newTodoElement);
-			
-			form.reset();
-			
-			//console.log(elementIdCount);
-			
-			Buttons[elementIdCount-1] = new Array(2);
-			Buttons[elementIdCount-1][0] = document.getElementById(`done-button-todo-element-${elementIdCount}`);
-			Buttons[elementIdCount-1][1] = document.getElementById(`edit-button-todo-element-${elementIdCount}`);
-			Buttons[elementIdCount-1][2] = document.getElementById(`delete-button-todo-element-${elementIdCount}`);
-			
-			//console.log(Buttons);
-			
-			Buttons[elementIdCount-1][0].addEventListener('click', function(event){
-				var id = event.target.id;
-				var idNumeric = id.replace( /^\D+/g, '');
-				onClick("Done", idNumeric);
-			});
+	elementIdCount = elementIdCount + 1;
+	newTodoElement.setAttribute("id", `done-todo-element-${elementIdCount}`);
+	defineTaskButtons();
 
-			Buttons[elementIdCount-1][1].addEventListener('click', function(event){
-				var id = event.target.id;
-				var idNumeric = id.replace( /^\D+/g, '');
-				onClick("Edit", idNumeric);
-			});
+	//ADDING TASK VALUE AND BUTTONS DIV
+	newTodoElement.appendChild(document.createTextNode(newTaskValue));
+	newTodoElement.appendChild(newTodoElementButtonsDiv);
+	//ADDING ELEMENT TO LIST
+	todoList.appendChild(newTodoElement);
 
-			Buttons[elementIdCount-1][2].addEventListener('click', function(event){
-				var id = event.target.id;
-				var idNumeric = id.replace( /^\D+/g, '');
-				onClick("Delete", idNumeric);
-			});
+	form.reset();
 
-		} //END OF ADDING NEW LIST ELEMENT		
+	//console.log(elementIdCount);
+
+	buttons[elementIdCount - 1] = new Array(2);
+	buttons[elementIdCount - 1][0] = document.getElementById(`done-button-todo-element-${elementIdCount}`);
+	buttons[elementIdCount - 1][1] = document.getElementById(`edit-button-todo-element-${elementIdCount}`);
+	buttons[elementIdCount - 1][2] = document.getElementById(`delete-button-todo-element-${elementIdCount}`);
+
+	//console.log(Buttons);
+
+	buttons[elementIdCount - 1][0].addEventListener('click', function (event) {
+		var id = event.target.id;
+		var idNumeric = id.replace(/^\D+/g, '');
+		onClick("Done", idNumeric);
+	});
+
+	buttons[elementIdCount - 1][1].addEventListener('click', function (event) {
+		var id = event.target.id;
+		var idNumeric = id.replace(/^\D+/g, '');
+		onClick("Edit", idNumeric);
+	});
+
+	buttons[elementIdCount - 1][2].addEventListener('click', function (event) {
+		var id = event.target.id;
+		var idNumeric = id.replace(/^\D+/g, '');
+		onClick("Delete", idNumeric);
+	});
+
+} 
+
+function defineTaskButtons () {
+	//BUTTONS DIV
+	var newTodoElementButtonsDiv = document.createElement("div");
+	newTodoElementButtonsDiv.setAttribute("id", `buttons-div-${elementIdCount}`);
+
+	createButton('done', "fa-check-square-o");
+	createButton('edit', "fa-pencil-square-o");
+	createButton('delete', "fa-minus-square-o");
+}
+
+function createButton(type, iconClass) {
+	var newBtnWrapper = document.createElement("div");
+	newBtnWrapper.setAttribute("id", `buttons-div-${elementIdCount}`);
+
+	var newBtn = document.createElement("button");
+	newBtn.setAttribute("type", "button");
+	newBtn.setAttribute("class", `button ${type}-button`);
+	newBtn.setAttribute("id", `${type}-button-todo-element-${elementIdCount}`);
+
+	createBtnIcon(iconClass);
+	newBtn.appendChild(document.createTextNode(type));
+	newBtnWrapper.appendChild(newBtn);
+}
+
+function createBtnIcon(iconClass) {
+	var doneIcon = document.createElement("i");
+	doneIcon.setAttribute("class", `fa ${iconClass}`);
+	doneIcon.setAttribute("aria-hidden", "true");
+	newTodoElementButtonDone.appendChild(doneIcon);
+}
+
+function onClick (type, id) {
+	if (type === "Done") {
+		taskDone(id);
+	} else if (type === "Edit") {
+		taskEdit(id);
+	} else if (type === "Delete") {
+		taskDelete(id);
+	} else {
+		location.reload();
+		return 0;
 	}
+}
 
-	function onClick (type, id) {
-		if (type === "Done") {
-			taskDone(id);
-		} else if (type === "Edit") {
-			taskEdit(id);
-		} else if (type === "Delete") {
-			taskDelete(id);
-		} else {
-			location.reload();
-			return 0;
-		}
-	}
+function taskDone(id) {
+	console.log(`Task id ${id} is DONE, it will be moved to DONE list`);
+	
+	var doneChild = document.getElementById(`done-todo-element-${id}`);
+	var doneParent = document.getElementById('done-list');
+	doneParent.appendChild(doneChild);
 
-	function taskDone(id) {
-		console.log(`Task id ${id} is DONE, it will be moved to DONE list`);
-		
-		var doneChild = document.getElementById(`done-todo-element-${id}`);
-		var doneParent = document.getElementById('done-list');
-		doneParent.appendChild(doneChild);
+	var buttonsParent = document.getElementById(`buttons-div-${id}`);
+	var doneButtonDone = document.getElementById(`done-button-todo-element-${id}`);
+	var doneButtonEdit = document.getElementById(`edit-button-todo-element-${id}`);
 
-		var buttonsParent = document.getElementById(`buttons-div-${id}`);
-		var doneButtonDone = document.getElementById(`done-button-todo-element-${id}`);
-		var doneButtonEdit = document.getElementById(`edit-button-todo-element-${id}`);
+	buttonsParent.removeChild(doneButtonDone);
+	buttonsParent.removeChild(doneButtonEdit);
 
-		buttonsParent.removeChild(doneButtonDone);
-		buttonsParent.removeChild(doneButtonEdit);
+	doneChild.setAttribute("id", `done-task-${id}`);
+}
 
-		doneChild.setAttribute("id", `done-task-${id}`);
+function taskEdit(id) {
+	console.log(`You will be able to edit your task id ${id}`);
+	
+}
 
-		
-	}
+function taskDelete(id) {
+	console.log(`Task id ${id} will be deleted`);
 
-	function taskEdit(id) {
-		console.log(`You will be able to edit your task id ${id}`);
-		
-	}
-
-	function taskDelete(id) {
-		console.log(`Task id ${id} will be deleted`);
-
-		var isItDone = document.getElementById(`done-task-${id}`);
-		
-		if (isItDone) {
-			var parent = document.getElementById('done-list');
-			var child = document.getElementById(`done-task-${id}`);
-		} else {
-			var parent = document.getElementById('todo-list');
-			var child = document.getElementById(`done-todo-element-${id}`);
-		}
-
-		
-		parent.removeChild(child);
+	var isItDone = document.getElementById(`done-task-${id}`);
+	
+	if (isItDone) {
+		var parent = document.getElementById('done-list');
+		var child = document.getElementById(`done-task-${id}`);
+	} else {
+		var parent = document.getElementById('todo-list');
+		var child = document.getElementById(`done-todo-element-${id}`);
 	}
 
 	
-
-})();
+	parent.removeChild(child);
+}
