20b20508d13714b1c7dfb6f88eb1a7d1806f936c
nexarr <camil.faron@gmail.com>
2017-11-08 00:14:35 +0100

    Calculator/solution-v3 (jQuery + ES6) update

--- Calculator/solution-v3/src/js/app.js
+++ Calculator/solution-v3/src/js/app.js
@@ -1,196 +1,194 @@
 import $ from 'jquery';
 import './../scss/styles.scss';
 
-var resultField = document.getElementById("result");
-var numbers = [];
-var actions = [];
-var isNewNumber = true;
+let resultField = $('#result');
+let numbers = [];
+let actions = [];
+let isNewNumber = true;
 
 $(function () {
 	registerEvents();
 });
 
-	function registerEvents() {
-		$('#delete').on('click', handleDelete);
-		$('#equal').on('click', handleEqual);
-		$('#back').on('click', handleBack);
-		$('.calc-field').on('click', onCalculatedFieldClick);	
-	}
+function registerEvents() {
+	$('#delete').on('click', handleDelete);
+	$('#equal').on('click', handleEqual);
+	$('#back').on('click', handleBack);
+	$('.calc-field').on('click', onCalculatedFieldClick);	
+}
 
-	function onCalculatedFieldClick(event) {
-		var currentField = event.target;
-		var isNumberField = currentField.classList.contains('number-field');
-		var value = currentField.innerText;
+function onCalculatedFieldClick(event) {
+	let currentField = event.target;
+	let isNumberField = currentField.classList.contains('number-field');
+	let value = currentField.innerText;
 
-		displayCurrentExpression(value);
-		
-		if (isNumberField) {
-			handleNumberMark(value);
-		} else {
-			handleActionMark(value);
-		}
-
-		printCurrentStateinConsole();
-	}
-
-	function handleNumberMark(value) {
-		if (isNewNumber) {
-			numbers.push(value);
-		} else {
-			updateLastNumber(value);
-		}
-
-		isNewNumber = false;		
-	}
-
-	function handleBack(event) {
-		if (isNewNumber) {
-			actions.pop();
-		} else {
-			cutLastNumber();
-		}
-
-		resultField.innerText = resultField.innerText.slice(0, -1);
-	}
-
-	function cutLastNumber () {
-		var lastIndex = numbers.length - 1;
-		var lastNumber = numbers[numbers.length - 1];
-
-		if (lastNumber.length === 1) {
-			numbers.pop();
-		} else {
-			lastNumber = lastNumber.slice(0, -1);
-			numbers[lastIndex] = lastNumber;
-		}
-	}
-
-	function handleActionMark(value) {
-			if (isNewNumber == false) {
-				actions.push(value);
-				isNewNumber = true;
-			} else {
-				resultField.innerText = resultField.innerText.slice(0, -1);
-			}
-	}
-
-	function handleDelete(event) {
-		numbers.length = 0;
-		actions.length = 0;
-		clear();
-		isNewNumber = true;
-	}
-
-	function handleEqual(event) {
-		printCurrentStateinConsole();
+	displayCurrentExpression(value);
 	
-		var result = calculateResult();
-		clear();
-		displayCurrentExpression(result);
-
-		printCurrentStateinConsole();
+	if (isNumberField) {
+		handleNumberMark(value);
+	} else {
+		handleActionMark(value);
 	}
 
-	function calculateResult() {
-		var result = ""; 
-		var higherPriorityActionIndex = findIndexOfActionWithHigerPriority();
+	printCurrentStateinConsole();
+}
 
-		if (higherPriorityActionIndex !== null) {
-			calculateFromGivenIndex(higherPriorityActionIndex);
+function handleNumberMark(value) {
+	if (isNewNumber) {
+		numbers.push(value);
+	} else {
+		updateLastNumber(value);
+	}
 
-			if(numbers.length === 1) {
-				result = numbers[0];
-			} else {
-				return calculateResult();
-			}
+	isNewNumber = false;		
+}
+
+function handleBack(event) {
+	if (isNewNumber) {
+		actions.pop();
+	} else {
+		cutLastNumber();
+	}
+	resultField.text(resultField.text().slice(0, -1));
+}
+
+function cutLastNumber () {
+	let lastIndex = numbers.length - 1;
+	let lastNumber = numbers[numbers.length - 1];
+
+	if (lastNumber.length === 1) {
+		numbers.pop();
+	} else {
+		lastNumber = lastNumber.slice(0, -1);
+		numbers[lastIndex] = lastNumber;
+	}
+}
+
+function handleActionMark(value) {
+		if (isNewNumber == false) {
+			actions.push(value);
+			isNewNumber = true;
 		} else {
-			result = calculateCustomExpression();
+			resultField.text(resultField.text().slice(0, -1));
 		}
+}
 
-		return result;
+function handleDelete(event) {
+	numbers.length = 0;
+	actions.length = 0;
+	clear();
+	isNewNumber = true;
+}
+
+function handleEqual(event) {
+	printCurrentStateinConsole();
+
+	let result = calculateResult();
+	clear();
+	displayCurrentExpression(result);
+
+	printCurrentStateinConsole();
+}
+
+function calculateResult() {
+	let result = ""; 
+	let higherPriorityActionIndex = findIndexOfActionWithHigerPriority();
+
+	if (higherPriorityActionIndex !== null) {
+		calculateFromGivenIndex(higherPriorityActionIndex);
+
+		if(numbers.length === 1) {
+			result = numbers[0];
+		} else {
+			return calculateResult();
+		}
+	} else {
+		result = calculateCustomExpression();
 	}
 
-	function calculateCustomExpression() {
-		while(numbers.length > 1) {
-			calculateFromGivenIndex(0);
-		}
+	return result;
+}
 
-		return numbers[0];
+function calculateCustomExpression() {
+	while(numbers.length > 1) {
+		calculateFromGivenIndex(0);
 	}
 
-	function calculateFromGivenIndex(index) {
-		var number1Index = index;
-		var number2Index = index + 1;
-		var number1 = parseFloat(numbers[number1Index]);
-		var number2 = parseFloat(numbers[number2Index]);
-		var operation = actions[index];
+	return numbers[0];
+}
 
-		if (!isNaN(number1) && !isNaN(number2)) {
-			var newNumber = simpleCalculate(number1, number2, operation);
-			numbers[number1Index] = newNumber;
-			removeElementFromList(numbers, number2Index);
-		}
-		
-		removeElementFromList(actions, index);
+function calculateFromGivenIndex(index) {
+	let number1Index = index;
+	let number2Index = index + 1;
+	let number1 = parseFloat(numbers[number1Index]);
+	let number2 = parseFloat(numbers[number2Index]);
+	let operation = actions[index];
+
+	if (!isNaN(number1) && !isNaN(number2)) {
+		let newNumber = simpleCalculate(number1, number2, operation);
+		numbers[number1Index] = newNumber;
+		removeElementFromList(numbers, number2Index);
 	}
 	
-	function updateLastNumber(value) {
-		var lastIdx = numbers.length - 1;
-		var lastNumber = numbers[lastIdx];
-		var nextNumber = lastNumber + value;
-		numbers[lastIdx] = nextNumber;
-	}
+	removeElementFromList(actions, index);
+}
 
-	function displayCurrentExpression(value) {
-		resultField.innerText = resultField.innerText + value;
-	}
+function updateLastNumber(value) {
+	let lastIdx = numbers.length - 1;
+	let lastNumber = numbers[lastIdx];
+	let nextNumber = lastNumber + value;
+	numbers[lastIdx] = nextNumber;
+}
 
-	function clear() {
-		printCurrentStateinConsole();
-		$('#result').text('');
-	}
+function displayCurrentExpression(value) {
+	console.log(resultField);
+	resultField.text(resultField.text() + value);
+}
 
-	function removeElementFromList(list, index) {
-		list.splice(index, 1)
-	}
+function clear() {
+	printCurrentStateinConsole();
+	$('#result').text('');
+}
 
-	function simpleCalculate(number1, number2, action) {
-		var result = "";
+function removeElementFromList(list, index) {
+	list.splice(index, 1)
+}
 
-			if (action == '+') {
-				result = number1 + number2;
-			} else if (action == '-') {
-				result = number1 - number2;
-			} else if (action == '*') {
-				result = number1 * number2;
-			} else if (action == ':') {
-				result = number1 / number2;
-			}
-			
-		return "" + result;
-	}
+function simpleCalculate(number1, number2, action) {
+	let result = "";
 
-	function findIndexOfActionWithHigerPriority() {
-		var higherPriorityActionIndex = null;
-
-		for (var i = 0; i < actions.length; i++) {
-			var action = actions[i];
-			if (action === '*' || action === ':') {
-				higherPriorityActionIndex = i;
-				break;
-			}
+		if (action === '+') {
+			result = number1 + number2;
+		} else if (action === '-') {
+			result = number1 - number2;
+		} else if (action === '*') {
+			result = number1 * number2;
+		} else if (action === ':') {
+			result = number1 / number2;
 		}
+		
+	return "" + result;
+}
 
-		return higherPriorityActionIndex;
+function findIndexOfActionWithHigerPriority() {
+	let higherPriorityActionIndex = null;
+
+	for (let value of actions) {
+		let action = actions[value];
+		
+		if (action === '*' || action === ':') {
+			higherPriorityActionIndex = value;
+			break;
+		}
 	}
 
+	return higherPriorityActionIndex;
+}
 
-
-	// helper function which log in console current state
-	function printCurrentStateinConsole() {
-		console.log('numbers: ', numbers);
-		console.log('actions: ', actions);
-		console.log('------------------')
-
-		console.log("isNewNumber = "+isNewNumber);
-	}
+// helper function which log in console current state
+function printCurrentStateinConsole() {
+	console.log(`numbers: ${numbers}`);
+	console.log(`actions: ${actions}`);
+	console.log('------------------')
+	console.log(`isNewNumber = ${isNewNumber}`);
+}
