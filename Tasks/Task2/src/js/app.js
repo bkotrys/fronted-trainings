2ee62db93f3f7900aad89f9ad55e1fa970097a57
nexarr <camil.faron@gmail.com>
2017-11-08 00:20:46 +0100

    Tasks/Task2 update

--- Tasks/Task2/src/js/app.js
+++ Tasks/Task2/src/js/app.js
@@ -3,10 +3,10 @@ var moment = require('moment');
 var date1 = moment("2010-01-01", "YYYY-MM-DD");
 var date2 = moment("2017-12-31", "YYYY-MM-DD");
 
-isFriday();
+checkNextDay();
 
-function isFriday() {
-	if (date1.day() === 5) {
+function checkNextDay() {
+	if (isFriday()) {
 		if (date1 < date2) {
 			isCorrectDayOfMonth();
 		} else {
@@ -14,7 +14,7 @@ function isFriday() {
 		}
 	} else {
 		date1.add(1, 'days');
-		isFriday();
+		checkNextDay();
 	}
 }
 
@@ -22,9 +22,17 @@ function isCorrectDayOfMonth() {
 	if (date1.date() === 5 || date1.date() === 10 || date1.date() ===20) {
 		console.log(`Date ${date1.format('YYYY-MM-DD')} is a ${date1.day()}'th day of the week.`);
 		date1.add(1, 'days');
-		isFriday();
+		checkNextDay();
 	} else {
 		date1.add(1, 'days');
-		isFriday();
+		checkNextDay();
+	}
+}
+
+function isFriday() {
+	if (date1.day() === 5) {
+		return true;
+	} else {
+		return false;
 	}
 }
